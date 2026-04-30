63ec44a051742ac97a82533d667858c81b0f0940
ali <m4567s019283@gmail.com>
2026-04-30 23:18:49 +0300

    cdd

--- components/mainData/Home.tsx
+++ components/mainData/Home.tsx
@@ -58,7 +58,7 @@ const getData=  async () => {
     ?allPokemon?.filter((pokemon: any) => pokemon.name.toLowerCase().includes(search))
     :dataType 
     ?dataType 
-    :data?.results
+    :data
   return loading?(
   <section className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
   {[...Array(20)].map((_, i) => (
