1cb5bfdd5e0f84b7eb52bee56a2b3a3689b3431b
ali <m4567s019283@gmail.com>
2026-04-30 18:38:31 +0300

    c

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="bg-gray-50 min-h-screen">
       <div className="max-w-7xl py-10 px-4">
-        <h1 className="text-2xl text-[#b43a87] font-semibold md:text-3xl">
+        <h1 className="text-2xl text-black font-semibold md:text-3xl">
           Welcome to PokeDex
         </h1>
         <MainData />;
